55c1bbc011f3ffb0b148ac5e437fd641f81ec73d
Anuarg Choudhary <anurag@Anuargs-MacBook-Air.local>
2022-09-10 02:31:08 +0530

    testing

--- test.cpp
+++ test.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<numeric>
+#include<sstream>
 using namespace std;
 int main(){
-
-    cout<<"hello world";
+    
     return 0;
 }
