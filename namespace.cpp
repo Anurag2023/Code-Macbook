e0b6383569c14c009fe3491a64549fbdad149723
Anurag Choudhary <panigale215@gmail.com>
2022-09-15 13:46:10 +0530

    count

--- namespace.cpp
+++ namespace.cpp
@@ -1,10 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void fun(int& a,int& b,int& c,int n)
+{
+    if(n==0)
+        return;
+    a++;
+    fun(a,b,c,n-1);
+    b++;
+    fun(a,b,c,n-1);
+    c++;
+    
+}
+
 int main(){
-    cout<<"hello world ";
-    int a=5;
-    cout<<a<<" ";
-    //int cout = 5;
-    cout<<" ";
+    int count = 0;
+    int a=0,b=1,c=2,n=5;
+    fun(a,b,c,n);
+    cout<<a<<" "<<b<<" "<<c<<" ";
     return 0;
 }
