c9976aec31135506efa2ac73ddff8b475420e194
Anurag Choudhary <panigale215@gmail.com>
2022-09-17 21:03:32 +0530

    rev stack

---
+++ revstck.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<numeric>
+#include<sstream>
+#include<stack>
+using namespace std;
+void insert(stack<int>&s,int temp){
+    if(s.empty()){
+        s.push(temp);
+        return;
+    }
+    int ele = s.top();
+    s.pop();
+    insert(s,temp);
+    s.push(ele);
+}
+void rev(stack<int>&s){
+    if(s.size()==1)
+    {
+        return;
+    }
+    int temp = s.top();
+    s.pop();
+    rev(s);
+    insert(s,temp);
+}
+int main()
+{
+    stack<int>s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.push(6);
+    s.push(7);
+    int a = s.top();
+    rev(s);
+    while(!s.empty())
+    {
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+
+    return 0;
+}
