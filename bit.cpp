ca973cb0ef089b87dd6c6e5658f0d4fdb54fd710
Anurag Choudhary <panigale215@gmail.com>
2022-09-21 09:58:32 +0530

    bitManipulation

---
+++ bit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<numeric>
+#include<sstream>
+using namespace std;
+int main(){
+    int x = 100 ,y;
+    y = x>>2;
+
+    int z = y|5;
+    cout<<z;
+    return 0;
+}
