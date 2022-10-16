644f101f393330a72e3bbd7266a910af84a07af0
Anurag Choudhary <panigale215@gmail.com>
2022-10-16 15:52:14 +0530

    cp

--- Codeforces/1585A.cpp
+++ Codeforces/1585A.cpp
@@ -10,10 +10,26 @@ int main(){
         for(int i = 0 ;i < n ; i++){
             cin>>arr[i];
         }
-        int sum = 0;
+        int sum = 1;
+        bool flag = false;
         for(int i = 0 ; i < n ; i++){
-            
+            if(arr[i]==1){
+                sum+=1;               
+                while(i<n-1 && arr[i+1] == 1){
+                    sum+=5;
+                    i++;
+                }
+            }
+            else if(arr[i] == 0 && i < (n-1) && arr[i+1]==0){
+                cout<<"-1"<<"\n";
+                flag = true;
+                break;
+            }
         }
+        if(flag){
+            continue;
+        }
+        cout<<sum<<"\n";
     }
     return 0;
 }
