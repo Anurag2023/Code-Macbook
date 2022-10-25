a24d6996eca709094d9629f7a08be2eb155b465d
Anurag Choudhary <panigale215@gmail.com>
2022-10-26 00:25:08 +0530

    codeforces contest

--- 1Sublime-Text/a.cpp
+++ 1Sublime-Text/a.cpp
@@ -2,11 +2,35 @@
 using namespace std;
 
 void solve(){
-	cout<<"hello i am inside solve\n";
+	stack<int>st;
+	int n;
+	cin>>n;
+	string s;
+	cin>>s;
+
+	for(int i = 0;i<n;i++){
+		if(s[i]=='Q'){
+			st.push(1);
+		}
+		else{
+			if(st.empty()){
+				continue;
+			}
+			else{
+				st.pop();
+			}
+		}
+	}
+	if(st.empty()){
+		cout<<"YES"<<"\n";
+	}
+	else
+		cout<<"NO"<<"\n";
 }
 
 int main(){
-	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 	int t;
 	cin>>t;
 	while(t--){
