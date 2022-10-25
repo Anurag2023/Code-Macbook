f3aefbca224b4d91dbc351d3a0fa0e4c0d19f639
Anurag Choudhary <panigale215@gmail.com>
2022-10-25 13:45:15 +0530

    template

--- 1Sublime-Text/a.cpp
+++ 1Sublime-Text/a.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void solve(){
+	cout<<"hello i am inside solve\n";
+}
+
 int main(){
-	cout<<"hello world";
+	
+	int t;
+	cin>>t;
+	while(t--){
+		solve();
+	}
 	return 0;
 }
