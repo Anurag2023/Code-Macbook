8ee0e9176616751836324c5ca48535f2d96d2197
Anurag Choudhary <panigale215@gmail.com>
2022-10-28 19:26:11 +0530

    define

--- 1Sublime-Text/test.cpp
+++ 1Sublime-Text/test.cpp
@@ -1,8 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main() {
-	int t;
-	cin >> t;
-	cout << (t * 100);
+	int n;
+	int a = 2,b=14,c=3;
+	if(a&&b>c){
+		cout<<"inside "<<a&&b;
+	}
+	else{
+		cout<<"hi";
+	}
+
 	return 0;
 }
