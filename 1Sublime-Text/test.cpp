a9e7622cd2293e5277770cf9adda254fa3d5ef49
Anurag Choudhary <panigale215@gmail.com>
2022-11-27 18:10:08 +0530

    recursion playlist

--- 1Sublime-Text/test.cpp
+++ 1Sublime-Text/test.cpp
@@ -4,6 +4,17 @@ int main(){
     int n;
     cin>>n;
     vector<int>nums(n);
+    for(int i = 0 ; i < n ; i++) cin>>nums[i];
+    int ans;
+    cin>>ans;
+    sort(nums.begin(),nums.end());
+    int l=0,r=n-1;
+    vector<pair<int,int>>vec;
     
-    
+    while(l<r){
+        if(nums[l]+nums[r]==ans){
+            //vector<int>temp;
+            vec.push_back(make_pair(nums[l],nums[r]));
+        }
+    }
 }
