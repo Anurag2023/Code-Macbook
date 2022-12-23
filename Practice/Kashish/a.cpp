a18709b80e42c07875472bd71fcbd0c4b22c3df4
Anurag Choudhary <panigale215@gmail.com>
2022-12-23 12:09:36 +0530

    publisys

---
+++ Practice/Kashish/a.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<numeric>
+#include<sstream>
+using namespace std;
+int main(){
+    
+    return 0;
+}
---
+++ Practice/Kashish/abhis.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<numeric>
+#include<sstream>
+using namespace std;
+int main(){
+    ccout<<"hi";
+    return 0;
+}
