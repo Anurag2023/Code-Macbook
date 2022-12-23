329fd9d21503bdf0745d8e1e73bf54e1b08a70b9
Anurag Choudhary <panigale215@gmail.com>
2022-12-23 12:11:30 +0530

    placements companies

---
+++ Practice/Random/abcv.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<numeric>
+#include<sstream>
+using namespace std;
+int main(){
+    cout<<"hello world";
+    int a = 10,b=-5;
+    int ans = (-b+a);
+    cout<<endl<<ans;
+    return 0;
+}
