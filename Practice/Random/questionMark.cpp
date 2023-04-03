3b0cdfcf7a0c48b0d28ccf32b1cbb04a0b0c316a
Anurag Choudhary <panigale215@gmail.com>
2023-04-03 12:39:46 +0530

    practice

---
+++ Practice/Random/questionMark.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<numeric>
+#include<sstream>
+using namespace std;
+
+bool fun(string s){
+    
+}
+
+int main(){
+    string s;
+    getline(cin,s);
+    bool ans = fun(s);
+    cout<<ans;
+    return 0;
+}
